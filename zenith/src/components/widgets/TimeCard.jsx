bf8bbeff5f11638aae3588ee28c3adbe3fadb832
Andrew Stevenson <129066987+AndrewStevenson550@users.noreply.github.com>
2026-02-05 18:11:03 +0000

    added the weather card and finished the time card

--- zenith/src/components/widgets/TimeCard.jsx
+++ zenith/src/components/widgets/TimeCard.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
-
+import { Clock } from "lucide-react"
 function TimeCard() {
     // 1. Initialize state with the current time
     const [time, setTime] = useState(new Date());
-
+    
     useEffect(() => {
         // 2. Set up an interval to update the state every second
         const timerId = setInterval(() => {
@@ -22,9 +22,21 @@ function TimeCard() {
     });
     
   return (
-    <div className='bg-time-card-bg rounded-lg text-center w-70 ml-10 h-80'>
-        <div>
-            <h2 className='font-inter font-[900] text-time-white'>{timeString}</h2>
+    <div className='relative bg-time-card-bg rounded-[12px] w-70 ml-10 h-60'>
+        {/* Icon Container */}
+        <div className='absolute top-6 left-7 justify-between flex gap-23 font-inter font-[700]'>
+            <Clock className=" text-clock-blue text-3xl"/>
+            <div className='bg-clock-blue-t rounded-4xl w-26 text-center text-time-white'>Local Time</div>
+        </div>
+
+        {/* Text Container */}
+        <div className='absolute bottom-10 left-5'>
+            <h2 className='font-inter font-[900] text-time-white text-5xl'>
+                {timeString}
+            </h2>
+            <h5>
+
+            </h5>
         </div>
     </div>
   )
