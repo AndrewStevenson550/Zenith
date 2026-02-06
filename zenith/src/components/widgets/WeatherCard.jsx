b0e43d3574efc0667178986a590c19474ef8e37d
Andrew Stevenson <129066987+AndrewStevenson550@users.noreply.github.com>
2026-02-06 18:40:45 +0000

    fixed a big with where the weather api icon would be stretched to small

--- zenith/src/components/widgets/WeatherCard.jsx
+++ zenith/src/components/widgets/WeatherCard.jsx
@@ -23,41 +23,41 @@ function WeatherCard() {
 
   return (
     <>
-      <div class="bg-[#1a222e] text-white p-8 rounded-2xl flex items-center max-w-2xl font-sans ml-105 -mt-50">
+      <div className="bg-[#1a222e] text-white p-8 rounded-2xl flex items-center max-w-3xl font-sans ml-105 -mt-50">
 
-        <div class="flex items-center gap-8 pr-12 border-r border-gray-700">
-          <div class="bg-[#2a2a24] p-6 rounded-full">
-            <img src={weather.current.condition.icon} alt="weather icon" class="w-64 " />
+        <div className="flex items-center gap-8 pr-20 border-r border-gray-700">
+          <div className="flex items-center justify-center bg-[#2a2a24] w-32 h-32 rounded-full">
+            <img src={weather.current.condition.icon} alt="weather icon" className="h-21 w-21 object-contain" />
           </div>
 
           <div>
-            <h2 class="text-gray-400 uppercase tracking-widest text-sm font-bold mb-1">
+            <h2 className="text-gray-400 uppercase tracking-widest text-sm font-bold mb-1">
               {weather.location.name}
             </h2>
-            <div class="flex items-center gap-6">
-              <h1 class="text-7xl font-black italic tracking-tighter">
+            <div className="flex items-center gap-6">
+              <h1 className="text-7xl font-black italic tracking-tighter">
                 {weather.current.temp_f}°
               </h1>
-              <div class="leading-tight">
-                <p class="text-3xl font-black uppercase tracking-tight">Clear</p>
-                <p class="text-3xl font-black uppercase tracking-tight">Sky</p>
+              <div className="leading-tight">
+                <p className="text-3xl font-black uppercase tracking-tight">Clear</p>
+                <p className="text-3xl font-black uppercase tracking-tight">Sky</p>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="flex gap-12 pl-12">
-          <div class="flex flex-col items-center text-center">
-            <div class="mb-2 text-gray-400">💨</div>
-            <p class="text-xl font-bold leading-none">{weather.current.wind_mph}</p>
-            <p class="text-xl font-bold mb-1">mph</p>
-            <p class="text-[10px] uppercase tracking-widest text-gray-400 font-bold">Wind</p>
+        <div className="flex gap-12 pl-12">
+          <div className="flex flex-col items-center text-center">
+            <div className="mb-2 text-gray-400">💨</div>
+            <p className="text-xl font-bold leading-none">{weather.current.wind_mph}</p>
+            <p className="text-xl font-bold mb-1">mph</p>
+            <p className="text-[10px] uppercase tracking-widest text-gray-400 font-bold">Wind</p>
           </div>
 
-          <div class="flex flex-col items-center text-center">
-            <div class="mb-2 text-gray-400 text-xl">💧</div>
-            <p class="text-xl font-bold mb-1">{weather.current.humidity}%</p>
-            <p class="text-[10px] uppercase tracking-widest text-gray-400 font-bold">Humidity</p>
+          <div className="flex flex-col items-center text-center">
+            <div className="mb-2 text-gray-400 text-xl">💧</div>
+            <p className="text-xl font-bold mb-1">{weather.current.humidity}%</p>
+            <p className="text-[10px] uppercase tracking-widest text-gray-400 font-bold">Humidity</p>
           </div>
         </div>
 
